e3d21d89102c2de94cad6c77a2d50f3d00eba89c
Felipe Elias Philipp <felipeelias@gmail.com>
2009-11-17 18:40:44 -0200

    Tests passed

--- prime_factors/lib/prime_factors.rb
+++ prime_factors/lib/prime_factors.rb
@@ -1,3 +1,17 @@
 class PrimeFactors
-  
+  def self.generate(number)
+    list = []
+    if number > 1
+      while number % 2 == 0
+        list << 2
+        number /= 2
+      end
+      while number % 3 == 0
+        list << 3
+        number /= 3
+      end
+    end
+    list << number if number > 1
+    list
+  end  
 end
--- prime_factors/spec/prime_factors_spec.rb
+++ prime_factors/spec/prime_factors_spec.rb
@@ -2,9 +2,34 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'prime_factors'))
 
 describe PrimeFactors do
-  context "test" do
-    it "should do" do
-      1.should == 1
+  
+  context "returning a prime factor of" do
+    it "1 should be an empty list" do
+      PrimeFactors.generate(1).should == []
+    end
+
+    it "2 should be a list with [2]" do
+      PrimeFactors.generate(2).should == [2]
+    end
+
+    it "3 should be a list with [3]" do
+      PrimeFactors.generate(3).should == [3]
+    end
+    
+    it "4 should be a list with [2, 2]" do
+      PrimeFactors.generate(4).should == [2, 2]
+    end
+
+    it "6 should be a list with [2, 3]" do
+      PrimeFactors.generate(6).should == [2, 3]
+    end
+    
+    it "8 should be a list with [2, 2, 2]" do
+       PrimeFactors.generate(8).should == [2, 2, 2]
+    end
+
+    it "9 should be a list with [3, 3]" do
+       PrimeFactors.generate(9).should == [3, 3]
     end
   end
 end
