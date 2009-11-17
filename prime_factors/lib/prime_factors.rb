ca709b75bfc914c0115a67d593522e578e9811f3
Felipe Elias Philipp <felipeelias@gmail.com>
2009-11-17 19:04:38 -0200

    Refactor

--- prime_factors/lib/prime_factors.rb
+++ prime_factors/lib/prime_factors.rb
@@ -2,13 +2,13 @@ class PrimeFactors
   def self.generate(number)
     list = []
     if number > 1
-      while number % 2 == 0
-        list << 2
-        number /= 2
-      end
-      while number % 3 == 0
-        list << 3
-        number /= 3
+      candidate = 2
+      while number > 1
+        while number % candidate == 0
+          list << candidate
+          number /= candidate
+        end
+        candidate += 1
       end
     end
     list << number if number > 1
