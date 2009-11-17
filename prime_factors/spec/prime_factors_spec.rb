af128476812048b5931d206f44f2c807ceb8cd68
Felipe Elias Philipp <felipeelias@gmail.com>
2009-11-17 17:48:18 -0200

    Inital commit -> PrimeFactorsKata

---
+++ prime_factors/spec/prime_factors_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'prime_factors'))
+
+describe PrimeFactors do
+  context "test" do
+    it "should do" do
+      1.should == 1
+    end
+  end
+end
