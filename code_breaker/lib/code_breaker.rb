8ef68ed7826c2666c2e8cfdffa43f1d21276d967
Felipe Elias Philipp <felipeelias@gmail.com>
2009-11-18 18:17:27 -0200

    Refactor to add spec matcher

--- code_breaker/lib/code_breaker.rb
+++ code_breaker/lib/code_breaker.rb
@@ -5,18 +5,22 @@ module CodeBreaker
     end
     
     def guess(combination)
-      ["m"] * number_of_non_positional_matches(combination) + 
-      ["p"] * number_of_positional_matches(combination)
+      @guess = combination
+    end
+    
+    def mark
+      ["m"] * number_of_non_positional_matches + 
+      ["p"] * number_of_positional_matches
     end
     
     private
     
-    def number_of_non_positional_matches(combination)
-      (@secret & combination).size - number_of_positional_matches(combination)
+    def number_of_non_positional_matches
+      (@secret & @guess).size - number_of_positional_matches
     end
     
-    def number_of_positional_matches(combination)
-      [@secret, combination].transpose.inject(0) do |sum, colors|
+    def number_of_positional_matches
+      [@secret, @guess].transpose.inject(0) do |sum, colors|
         sum + (colors[0] == colors[1] ? 1 : 0)
       end
     end
--- code_breaker/spec/code_breaker_spec.rb
+++ code_breaker/spec/code_breaker_spec.rb
@@ -3,6 +3,21 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'code_br
 
 include CodeBreaker
 
+Spec::Matchers.define :mark do |guess|
+  match do |marker|
+    marker.guess(guess)
+    @actual_mark = marker.mark
+    @actual_mark == @expected_mark
+  end
+  def and_return(mark)
+    @expected_mark = mark
+    self
+  end
+  failure_message_for_should do |marker|
+    "#{@guess.inspect} expected to mark as:\n#{@mark.inspect}, but got\n#{@actual_mark.inspect}"
+  end
+end
+
 describe Marker do
   
   before(:all) do
@@ -11,57 +26,57 @@ describe Marker do
   
   context "0 maches" do
     it "return []" do
-      @marker.guess(%w[w w w w]).should == []
+      @marker.should mark(%w[w w w w]).and_return([])
     end
   end
   
   context "only right colors in wrong place" do
     it "1 color return [m]" do
-      @marker.guess(%w[w w w r]).should == %w[m]
+      @marker.should mark(%w[w w w r]).and_return(%w[m])
     end
 
     it "2 colors return [m m]" do
-      @marker.guess(%w[w w g r]).should == %w[m m]
+      @marker.should mark(%w[w w g r]).and_return(%w[m m])
     end
 
     it "3 colors return [m m m]" do
-      @marker.guess(%w[w b g r]).should == %w[m m m]
+      @marker.should mark(%w[w b g r]).and_return(%w[m m m])
     end
 
     it "all colors return [m m m m]" do
-      @marker.guess(%w[y b g r]).should == %w[m m m m]
+      @marker.should mark(%w[y b g r]).and_return(%w[m m m m])
     end
   end
   
   context "only right colors in right place" do
     it "1 color return [p]" do
-      @marker.guess(%w[w w w y]).should == %w[p]
+      @marker.should mark(%w[w w w y]).and_return(%w[p])
     end
 
     it "2 colors return [p p]" do
-      @marker.guess(%w[w w b y]).should == %w[p p]
+      @marker.should mark(%w[w w b y]).and_return(%w[p p])
     end
 
     it "3 colors return [p p p]" do
-      @marker.guess(%w[w g b y]).should == %w[p p p]
+      @marker.should mark(%w[w g b y]).and_return(%w[p p p])
     end
 
     it "all colors return [p p p p]" do
-      @marker.guess(%w[r g b y]).should == %w[p p p p]
+      @marker.should mark(%w[r g b y]).and_return(%w[p p p p])
     end
   end  
 
   context "mixed right colors in right/wrong place" do
     it "1 right color/place and 1 wrong place return [m p]" do
-      @marker.guess(%w[r b w w]).should == %w[m p]
+      @marker.should mark(%w[r b w w]).and_return(%w[m p])
     end
 
     it "2 right color/place and 2 wrong place return [m m p p]" do
-      @marker.guess(%w[r y b g]).should == %w[m m p p]
+      @marker.should mark(%w[r y b g]).and_return(%w[m m p p])
     end
 
     it "2 right color/place and 2 wrong place return [m m p p] (inverse order)" do
-      @marker.guess(%w[b g r y]).should == %w[m m p p]
+      @marker.should mark(%w[b g r y]).and_return(%w[m m p p])
     end
   end  
 
