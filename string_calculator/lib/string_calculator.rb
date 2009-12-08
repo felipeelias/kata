830d80dd570aa191b88f6946920a6ffa3efc2259
Felipe Elias Philipp <felipeelias@gmail.com>
2009-12-08 01:12:45 -0200

    Tests for 4 numbers

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -2,7 +2,7 @@ module StringCalculator
   def add
     return 0 if self.empty?
     return self.to_i if numbers.length == 1
-    numbers[0] + numbers[1]
+    numbers.inject { |sum, number| sum += number }
   end
   
   private
--- string_calculator/spec/string_calculator_spec.rb
+++ string_calculator/spec/string_calculator_spec.rb
@@ -3,7 +3,11 @@ require 'string_calculator'
 
 Spec::Matchers.define :add do |sum|
   match do |actual|
-    actual.extend(StringCalculator).add == sum
+    (@result = actual.extend(StringCalculator).add) == sum
+  end
+  
+  failure_message_for_should do |string|
+    "Expected #{string} add #{sum} but got #{@result}"
   end
 end
 
@@ -33,6 +37,20 @@ describe StringCalculator do
 
     it "returns 127 for 100,27" do
       "100,27".should add(127)
+    end
+  end
+
+  context "3 numbers" do
+    it "returns 9 for 2,3,4" do
+      "2,3,4".should add(9)
+    end
+
+    it "returns 37 for 10,15,12" do
+      "10,15,12".should add(37)
+    end
+    
+    it "returns 179 for 100,27,52" do
+      "100,27,52".should add(179)
     end
   end
 end
