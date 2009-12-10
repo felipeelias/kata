535d17ae1ed3ff414d975f613860a9fbd0cb9e41
Felipe Elias Philipp <felipeelias@gmail.com>
2009-12-09 22:20:01 -0200

    refactor

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -1,8 +1,7 @@
 module StringCalculator
   def add
+    return 0 if empty?
     raise_if_negative_numbers
-    return 0 if self.empty?
-    return self.to_i if numbers.length == 1
     numbers.inject { |sum, number| sum += number }
   end
   
